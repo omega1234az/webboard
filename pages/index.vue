051b2250246cf3a171123cfd0918a407c6fa3b9e
omega1234az <supakit0920@gmail.com>
2024-07-17 04:13:55 +0700

    add img

--- pages/index.vue
+++ pages/index.vue
@@ -26,32 +26,27 @@
         </div>
         <div class="w-full h-[5px] bg-[#3A3939] my-5 rounded-md"></div>
         <div class="w-full flex gap-10">
+
           <div class="w-[60%] flex flex-col gap-5">
             <div class="w-full  bg-[#3A3939] rounded-3xl px-5 py-5">
               <div class="text-white flex w-full">
                 <img class="w-[40px] object-contain rounded-full h-[40px] bg-white"
                   src="https://images.immediate.co.uk/production/volatile/sites/3/2023/03/Jujutsu-Kaisen-Cropped-dbe733b.jpg?quality=90&resize=844,563"
                   alt="">
-                <p class="text-white ml-5 justify-center items-center flex">Gojo Satoru</p>
+                <p class="text-white ml-3 justify-center items-center flex">Gojo Satoru</p>
               </div>
-              <div class="text-white flex w-full text-2xl mt-2">แนะนำหนังซีรี่ย์น่าดูหน่อยคะ</div>
+              <div class="text-white flex w-full text-2xl mt-2">แมวพันธ์ไหนเนื้ออร่อยที่สุด</div>
               <div class="flex mt-3 gap-2 text-lg">
                 <div class="bg-[#202020] rounded-3xl flex px-5 py-2 gap-2 items-center">
-                  <img class="object-contain rounded-full w-5 h-5 bg-slate-50"
-                    src="https://images.immediate.co.uk/production/volatile/sites/3/2023/03/Jujutsu-Kaisen-Cropped-dbe733b.jpg?quality=90&resize=844,563"
-                    alt="">
+                  <img class="object-contain rounded-full w-5 h-5 " src="../public/img/comment.png" alt="">
                   <p class="text-[#8B8B8B] font-bold">5</p>
                 </div>
                 <div class="bg-[#202020] rounded-3xl flex px-5 py-2 gap-2 items-center">
-                  <img class="object-contain rounded-full w-5 h-5 bg-slate-50"
-                    src="https://images.immediate.co.uk/production/volatile/sites/3/2023/03/Jujutsu-Kaisen-Cropped-dbe733b.jpg?quality=90&resize=844,563"
-                    alt="">
+                  <img class="object-contain rounded-full w-5 h-5 " src="../public/img/save.png" alt="">
                   <p class="text-[#8B8B8B] font-bold">Save</p>
                 </div>
                 <div class="bg-[#202020] rounded-3xl flex px-5 py-2 gap-2 items-center">
-                  <img class="object-contain rounded-full w-5 h-5 bg-slate-50"
-                    src="https://images.immediate.co.uk/production/volatile/sites/3/2023/03/Jujutsu-Kaisen-Cropped-dbe733b.jpg?quality=90&resize=844,563"
-                    alt="">
+                  <img class="object-contain rounded-full w-5 h-5 " src="../public/img/share.png" alt="">
                   <p class="text-[#8B8B8B] font-bold">Share</p>
                 </div>
               </div>
@@ -59,40 +54,59 @@
             <div class="w-full  bg-[#3A3939] rounded-3xl px-5 py-5">
               <div class="text-white flex w-full">
                 <img class="w-[40px] object-contain rounded-full h-[40px] bg-white"
-                  src="https://images.immediate.co.uk/production/volatile/sites/3/2023/03/Jujutsu-Kaisen-Cropped-dbe733b.jpg?quality=90&resize=844,563"
+                  src="https://pbs.twimg.com/media/FTZM5yzXwAEMX-K.jpg"
                   alt="">
-                <p class="text-white ml-5 justify-center items-center flex">Gojo Satoru</p>
+                <p class="text-white ml-3 justify-center items-center flex ">Kiyotaka Ayanokōji</p>
               </div>
-              <div class="text-white flex w-full text-2xl mt-2">แนะนำหนังซีรี่ย์น่าดูหน่อยคะ</div>
+              <div class="text-white flex w-full text-2xl mt-2 ">ขอวิธีเข้ามอแบบไม่ต้องขึ้นรถเมย์</div>
               <div class="flex mt-3 gap-2 text-lg">
                 <div class="bg-[#202020] rounded-3xl flex px-5 py-2 gap-2 items-center">
-                  <img class="object-contain rounded-full w-5 h-5 bg-slate-50"
-                    src="https://images.immediate.co.uk/production/volatile/sites/3/2023/03/Jujutsu-Kaisen-Cropped-dbe733b.jpg?quality=90&resize=844,563"
-                    alt="">
+                  <img class="object-contain rounded-full w-5 h-5 " src="../public/img/comment.png" alt="">
                   <p class="text-[#8B8B8B] font-bold">5</p>
                 </div>
                 <div class="bg-[#202020] rounded-3xl flex px-5 py-2 gap-2 items-center">
-                  <img class="object-contain rounded-full w-5 h-5 bg-slate-50"
-                    src="https://images.immediate.co.uk/production/volatile/sites/3/2023/03/Jujutsu-Kaisen-Cropped-dbe733b.jpg?quality=90&resize=844,563"
-                    alt="">
+                  <img class="object-contain rounded-full w-5 h-5 " src="../public/img/save.png" alt="">
                   <p class="text-[#8B8B8B] font-bold">Save</p>
                 </div>
                 <div class="bg-[#202020] rounded-3xl flex px-5 py-2 gap-2 items-center">
-                  <img class="object-contain rounded-full w-5 h-5 bg-slate-50"
-                    src="https://images.immediate.co.uk/production/volatile/sites/3/2023/03/Jujutsu-Kaisen-Cropped-dbe733b.jpg?quality=90&resize=844,563"
-                    alt="">
+                  <img class="object-contain rounded-full w-5 h-5 " src="../public/img/share.png" alt="">
                   <p class="text-[#8B8B8B] font-bold">Share</p>
                 </div>
               </div>
             </div>
+            <div class="w-full  bg-[#3A3939] rounded-3xl px-5 py-5">
+              <div class="text-white flex w-full">
+                <img class="w-[40px] object-contain rounded-full h-[40px] bg-white"
+                  src="https://pbs.twimg.com/media/ErEGc9BW8AQ5vDP.jpg"
+                  alt="">
+                <p class="text-white ml-3 justify-center items-center flex">คนไม่จำเป็น</p>
+              </div>
+              <div class="text-white flex w-full text-2xl mt-2">หาซื้อเฉาก๊วยชากังราวด่วนๆ</div>
+              <div class="flex mt-3 gap-2 text-lg">
+                <div class="bg-[#202020] rounded-3xl flex px-5 py-2 gap-2 items-center">
+                  <img class="object-contain rounded-full w-5 h-5 " src="../public/img/comment.png" alt="">
+                  <p class="text-[#8B8B8B] font-bold">5</p>
+                </div>
+                <div class="bg-[#202020] rounded-3xl flex px-5 py-2 gap-2 items-center">
+                  <img class="object-contain rounded-full w-5 h-5 " src="../public/img/save.png" alt="">
+                  <p class="text-[#8B8B8B] font-bold">Save</p>
+                </div>
+                <div class="bg-[#202020] rounded-3xl flex px-5 py-2 gap-2 items-center">
+                  <img class="object-contain rounded-full w-5 h-5 " src="../public/img/share.png" alt="">
+                  <p class="text-[#8B8B8B] font-bold">Share</p>
+                </div>
+              </div>
+            </div>
+
           </div>
-          
+
           <div class="w-[40%] ">
             <div
-              class="w-full h-[50px] bg-[#455DFF] rounded-md text-white text-2xl font-bold items-center justify-center flex">
+              class="w-full py-3 bg-[#455DFF] rounded-md text-white text-2xl font-bold items-center justify-center flex">
               POPULAR</div>
             <ul
-              class="w-full bg-[#3A3939] h-[500px] rounded-2xl mt-4 text-white text-2xl px-5 py-5 font-bold gap-7 flex flex-col">
+              class="w-full bg-[#3A3939] py-5 rounded-2xl mt-4 text-white text-2xl px-5 font-bold gap-7 flex flex-col">
+              <li>วิศวกรรมซอฟต์แวร์</li>
               <li>รถเมย์มอ</li>
               <li>ร้านเด็ดหน้ามอ</li>
               <li>เรื่องหลอนมพ</li>
